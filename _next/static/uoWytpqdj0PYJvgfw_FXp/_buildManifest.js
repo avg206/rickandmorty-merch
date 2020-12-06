5a75a208b32bb2f01065c0d7d96a82f1da96e430
avg206 <avg.tolik@gmail.com>
2020-12-06 19:22:22 +0000

    Deploying to gh-pages from  @ 365b2ec6dda0e3e995970f24734e7bde398b3381 🚀

---
+++ _next/static/uoWytpqdj0PYJvgfw_FXp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1515656c7d867f8467e7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d92afaa51e3248ec0437.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
